8e9afa8436f71428d5d3f923a6142760f53eca06
FinalProject <130982229+FinalProjectReact@users.noreply.github.com>
2023-05-02 21:11:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-// // import Login from './login/login';
-// // import { Outlet } from 'react-router';
-// // import { Link } from 'react-router-dom';
+import { Outlet } from 'react-router';
+import { Link } from 'react-router-dom';
+import Login from './login/login';
 
 function App() {
   return (
     <div className="App" id='App'>
-      
-      <h1>gjhfhghfghjhj</h1>
-
-{/* <Login/> */}
-
-
-     </div>
-
-    
+      <Login/>
+    </div>
   );
 }
 
